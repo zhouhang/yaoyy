0e1f13761692bea7ca290d6ec93eb0b39042a5ad
chaochaoxiao <376161552@qq.com>
2016-12-09 17:12:19 +0800

    0.2sql语句

---
+++ res/yaoyy/0.2.sql
@@ -0,0 +1,83 @@
+
+DROP TABLE IF EXISTS `follow_commodity`;
+CREATE TABLE `follow_commodity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `commodity_id` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for history_price
+-- ----------------------------
+DROP TABLE IF EXISTS `history_price`;
+CREATE TABLE `history_price` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `commodity_id` int(11) DEFAULT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `url` varchar(128) DEFAULT NULL,
+  `content` varchar(64) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `event_id` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for quotation
+-- ----------------------------
+DROP TABLE IF EXISTS `quotation`;
+CREATE TABLE `quotation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `status` int(5) DEFAULT NULL,
+  `content` text,
+  `description` text,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for supplier
+-- ----------------------------
+DROP TABLE IF EXISTS `supplier`;
+CREATE TABLE `supplier` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(11) DEFAULT NULL,
+  `phone2` varchar(20) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `area` varchar(255) DEFAULT NULL,
+  `enter_category` varchar(255) DEFAULT NULL,
+  `qq` varchar(255) DEFAULT NULL,
+  `mark` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `yaoyy`.`commodity`
+ADD COLUMN `price_update_time` DATETIME NULL COMMENT '价格更新时间' AFTER `supplier_id`;
+
+
+ALTER TABLE `commodity`
+MODIFY COLUMN `supplier_id`  int(11) NOT NULL AFTER `thumbnail_url`;
+
