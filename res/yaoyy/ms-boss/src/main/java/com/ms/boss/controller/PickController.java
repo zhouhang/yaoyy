22260fa83d48314e6aa6ad0211ed642962fc04b9
chaochaoxiao <376161552@qq.com>
2016-12-12 10:57:41 +0800

    选货单和寄样单状态改变是消费消息

--- res/yaoyy/ms-boss/src/main/java/com/ms/boss/controller/PickController.java
+++ res/yaoyy/ms-boss/src/main/java/com/ms/boss/controller/PickController.java
@@ -12,9 +12,12 @@ import com.ms.service.PickCommodityService;
 import com.ms.service.PickService;
 import com.ms.service.PickTrackingService;
 import com.ms.service.UserDetailService;
+import com.ms.service.enums.MessageEnum;
 import com.ms.service.enums.RedisEnum;
+import com.ms.service.observer.MsgConsumeEvent;
 import com.ms.tools.entity.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,6 +50,9 @@ public class PickController extends BaseController{
 
     @Autowired
     HttpSession httpSession;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     /**
      * 选货单列表
@@ -96,6 +102,10 @@ public class PickController extends BaseController{
     @ResponseBody
     private Result delete(Pick pick){
         pickService.update(pick);
+        if(pick.getAbandon()==1){
+            MsgConsumeEvent msgConsumeEvent=new MsgConsumeEvent(pick.getId(), MessageEnum.PICK);
+            applicationContext.publishEvent(msgConsumeEvent);
+        }
         return Result.success().msg("操作成功");
     }
 
--- res/yaoyy/ms-service/src/main/java/com/ms/service/observer/MsgConsumeListener.java
+++ res/yaoyy/ms-service/src/main/java/com/ms/service/observer/MsgConsumeListener.java
@@ -3,12 +3,14 @@ package com.ms.service.observer;
 import com.ms.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
 /**
  * Author: koabs
  * 12/5/16.
  * 生产消息监听器
  */
+@Component
 public class MsgConsumeListener implements ApplicationListener<MsgConsumeEvent> {
 
     @Autowired
