1a9044bac3711ed0f495fa273493216c9a402430
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2017-01-04 19:27:29 +0800

    [Summary]账单线下转账

--- res/yaoyy/ms-biz/src/main/java/com/ms/biz/controller/BillController.java
+++ res/yaoyy/ms-biz/src/main/java/com/ms/biz/controller/BillController.java
@@ -1,18 +1,17 @@
 package com.ms.biz.controller;
 
 import com.github.pagehelper.PageInfo;
+import com.ms.dao.enums.PickTrackingTypeEnum;
+import com.ms.dao.enums.SettleTypeEnum;
+import com.ms.dao.enums.TrackingTypeEnum;
 import com.ms.dao.model.Pick;
+import com.ms.dao.model.Setting;
 import com.ms.dao.model.User;
-import com.ms.dao.vo.AccountBillVo;
-import com.ms.dao.vo.PayRecordVo;
-import com.ms.dao.vo.PickVo;
-import com.ms.dao.vo.UserDetailVo;
-import com.ms.service.AccountBillService;
-import com.ms.service.PayRecordService;
-import com.ms.service.PickService;
-import com.ms.service.UserDetailService;
+import com.ms.dao.vo.*;
+import com.ms.service.*;
 import com.ms.service.enums.RedisEnum;
 import com.ms.tools.entity.Result;
+import com.ms.tools.exception.ControllerException;
 import com.ms.tools.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,6 +22,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Author: koabs
@@ -31,7 +33,7 @@ import javax.servlet.http.HttpSession;
  */
 @Controller
 @RequestMapping("/bill")
-public class BillController {
+public class BillController extends BaseController{
     @Autowired
     private HttpSession httpSession;
 
@@ -43,6 +45,9 @@ public class BillController {
 
     @Autowired
     private PickService pickService;
+
+    @Autowired
+    SettingService settingService;
 
     /**
      * 账单列表页面
@@ -73,7 +78,7 @@ public class BillController {
      * @param model
      * @return
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
     public  String detail(@PathVariable("id") Integer id, ModelMap model){
         User user = (User) httpSession.getAttribute(RedisEnum.USER_SESSION_BIZ.getValue());
         AccountBillVo accountBillVo=accountBillService.findVoById(id);
@@ -86,5 +91,77 @@ public class BillController {
         model.put("payRecord",payRecordVo);
         model.put("pick",pick);
         return "bill_detail";
+    }
+
+    /**
+     * 银行转账页面
+     * @param billId
+     * @return
+     */
+    @RequestMapping(value = "bankTransfer", method = RequestMethod.GET)
+    public String bankTransfer(Integer billId, ModelMap model){
+        // 根据订单Id 获取转账金额.同时确认订单属于当前用户
+        User user = (User) httpSession.getAttribute(RedisEnum.USER_SESSION_BIZ.getValue());
+        AccountBillVo bill=accountBillService.findVoById(billId);
+        if (!(bill!= null && bill.getUserId().equals(user.getId()))){
+            throw new ControllerException("用户无权限访问此付款页面.");
+        }
+        model.put("total",bill.getAmountsPayable()-bill.getAlreadyPayable());
+        PayRecordVo payRecord = payRecordService.findByBillId(billId);
+        if (payRecord != null) {
+            model.put("id", payRecord.getId());
+            if (payRecord.getPayDocuments()!= null && payRecord.getPayDocuments().size()>0)
+                model.put("url",payRecord.getPayDocuments().get(0).getPath());
+        }
+        // 后台配置的银行信息
+        Setting setting = settingService.find();
+        model.put("setting", setting);
+        model.put("billId", billId);
+        return "bill_pay";
+    }
+
+    /**
+     * 保存银行转账
+     * @param record
+     * @return
+     */
+    @RequestMapping(value = "bankTransfer", method = RequestMethod.POST)
+    public String bankTransferSave(PayRecordVo record, String url){
+        // 根据订单Id同时确认订单属于当前用户
+        User user = (User) httpSession.getAttribute(RedisEnum.USER_SESSION_BIZ.getValue());
+        AccountBillVo bill=accountBillService.findVoById(record.getAccountBillId());
+        if (!(bill!= null && bill.getUserId().equals(user.getId()))){
+            throw new ControllerException("用户无权限访问此付款页面.");
+        }
+
+        PayDocumentVo document = new PayDocumentVo();
+        document.setPath(url);
+        List<PayDocumentVo> list = new ArrayList<>();
+        list.add(document);
+        record.setPayDocuments(list);
+        record.setUserId(user.getId());
+        record.setCode(bill.getCode());
+        record.setCodeType(1);
+        record.setStatus(0);
+        record.setPayType(0);
+        // 设置默认信息
+        // 后台配置的银行信息
+        Setting setting = settingService.find();
+        record.setPayAccount(setting.getPayAccount());
+        record.setPayBankCard(setting.getPayBankCard());
+        record.setPayBank(setting.getPayBank());
+        // 判断之前没有支付记录TODO:
+        payRecordService.save(record);
+        return "redirect:/bill/bankTransferSuccess?billId="+ record.getAccountBillId();
+    }
+
+    /**
+     * 转账成功页面
+     * @return
+     */
+    @RequestMapping(value = "bankTransferSuccess", method = RequestMethod.GET)
+    public String bankTransferSuccess(Integer billId, ModelMap model){
+        model.put("billId",billId);
+        return "bill_pay_success";
     }
 }
---
+++ res/yaoyy/ms-biz/src/main/resources/templates/bill_pay.ftl
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>银行转账-药优优</title>
+    <#include "./common/meta.ftl"/>
+</head>
+<body class="ui-body-nofoot">
+<header class="ui-header">
+    <div class="title">银行转账</div>
+    <div class="abs-l mid">
+        <a href="/bill/detail/${billId!}" class="fa fa-back"></a>
+    </div>
+</header><!-- /ui-header -->
+
+<section class="ui-content">
+
+    <div class="pay">
+        <div class="hd">
+            <em>需付金额：<strong>&yen;${total?string}</strong>元</em>
+        </div>
+        <div class="bd">
+            <div class="row">
+                <div class="txt">支付方式：</div>
+                <div class="val">银行汇款</div>
+            </div>
+            <div class="row">
+                <div class="txt">账户名称：</div>
+                <div class="val">${setting.payAccount}</div>
+            </div>
+            <div class="row">
+                <div class="txt">帐号：</div>
+                <div class="val">${setting.payBankCard}</div>
+            </div>
+            <div class="row">
+                <div class="txt">开户行：</div>
+                <div class="val">${setting.payBank}</div>
+            </div>
+        </div>
+        <div class="ft">
+            <span>上传支付凭证：</span>
+            <span class="ui-file thumb">
+            <#if url?exists>
+                <img src="${url}" alt=""><i class="del"></i>
+            <#else >
+                <input type="file" name="file" accept="image/gif,image/jpeg,image/png" class="upfile" />
+            </#if>
+            </span>
+        </div>
+        <form id="bankTransfer" method="post" action="/bill/bankTransfer">
+        <input type="text" style="display: none" name="url" id="url" <#if url?exists>value="${url}" </#if> >
+        <input type="text" style="display: none" name="accountBillId" value="${billId!}">
+        <input type="text" style="display: none" name="id" <#if id?exists>value="${id}"</#if>>
+        <div class="button">
+            <button type="submit" class="ubtn ubtn-primary" id="submit">确认支付</button>
+        </div>
+        </form>
+    </div>
+
+</section><!-- /ui-content -->
+
+<#include "./common/footer.ftl"/>
+<script src="assets/js/lrz.bundle.js?id=1"></script>
+<script>
+    var _global = {
+        fn: {
+            init: function() {
+                this.submit();
+                this.upfile();
+                gallery();
+            },
+            submit: function() {
+                var self = this;
+                $('#bankTransfer').submit(function () {
+                    if ($("#url").val()== null || $("#url").val() == "") {
+                        popover('请上传转账凭证');
+                        return false;
+                    } else {
+                        return true;
+                    }
+                });
+            },
+            upfile: function() {
+                var $el = $('.ui-file');
+                var reset = function() {
+                    $el.html('<input type="file" name="file" accept="image/gif,image/jpeg,image/png" class="upfile" />');
+                    $("#url").val('');
+                }
+                var showLader = function() {
+                    $el.html('<i class="loader"></i>');
+                }
+                $el.on('change', '.upfile', function(ev) {
+                    showLader();
+                    lrz(this.files[0], {
+                        width: 800
+                    }).then(function (rst) {
+                        var base64 = rst.base64;
+                        base64 = base64.substr(base64.indexOf(',') + 1);
+                        $.ajax({
+                            url: "/pick/upload",
+                            data: {
+                                img: base64,
+                                fileName:rst.origin.name
+                            },
+                            type: 'POST',
+                            dataType: 'json',
+                            success: function (result) {
+                                if (result.status === 'success') {
+                                    $el.html('<img src="' + result.url + '" alt="" data-src="' + result.url + '"><i class="del"></i>');
+                                    $("#url").val(result.url);
+                                } else {
+                                    popover('上传图片失败，请刷新页面重试！');
+                                }
+                            },
+                            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                                popover('网络连接超时，请您稍后重试！');
+                            }
+                        })
+                    }).catch(function (err) {
+                        // 处理失败会执行
+                        reset();
+                    }).always(function () {
+                        // 不管是成功失败，都会执行
+                    });
+                });
+                $el.on('click', '.del', function() {
+                    reset();
+                })
+            }
+        }
+    }
+
+    $(function(){
+        _global.fn.init();
+    });
+
+</script>
+</body>
+</html>
