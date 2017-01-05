4afa89c4d7ac67a19dac096d1afd8ed289edb2b8
frank <frank1989678@126.com>
2017-01-05 17:16:39 +0800

    js判断微信修改

--- res/yaoyy/ms-biz/src/main/resources/static/assets/js/app.js
+++ res/yaoyy/ms-biz/src/main/resources/static/assets/js/app.js
@@ -142,14 +142,7 @@ var _YYY = {
     APPLYINFO: 'userInfo', // 申请寄样以及申请选货单提交的资料
     QUOTETIME:'quoteTime',//最近查看quote时间
 
-    is_weixn: (function() {
-        var ua = navigator.userAgent.toLowerCase();  
-        if(ua.match(/MicroMessenger/i)=="micromessenger") {  
-            return true;  
-        } else {  
-            return false;  
-        }  
-    })(),
+    is_weixn: /MicroMessenger/.test(navigator.userAgent),
 
     timeago: {
         fillZero: function(number) {
