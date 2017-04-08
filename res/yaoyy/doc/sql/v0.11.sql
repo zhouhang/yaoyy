e5cbadba5dc482eb6606f97946112b641177c1f0
wangbin <burgleaf@gmail.com>
2017-04-09 00:27:26 +0800

    提交sql语句问题

--- res/yaoyy/doc/sql/v0.11.sql
+++ res/yaoyy/doc/sql/v0.11.sql
@@ -5,10 +5,14 @@ ALTER TABLE `pick` ADD FOREIGN KEY (`supplier_id`) REFERENCES `user` (`id`);
 
 
 ALTER TABLE `pick`
-ADD COLUMN `complete_time`  datetime NULL COMMENT '订单完成时间' AFTER `update_time`
+ADD COLUMN `complete_time`  datetime NULL COMMENT '订单完成时间' AFTER `update_time`;
 
 ALTER TABLE `yaoyy`.`history_commodity`
 ADD COLUMN `category_id` INT NULL COMMENT '品种ID' AFTER `create_time`;
 ALTER TABLE `yaoyy`.`history_commodity`
 CHANGE COLUMN `picture_url` `picture_url` VARCHAR(255) CHARACTER SET 'utf8' NULL ,
 CHANGE COLUMN `thumbnail_url` `thumbnail_url` VARCHAR(255) CHARACTER SET 'utf8' NULL ;
+
+
+ALTER TABLE `user_detail`
+  MODIFY COLUMN `contract`  int(8) NULL DEFAULT 0 AFTER `company`;
